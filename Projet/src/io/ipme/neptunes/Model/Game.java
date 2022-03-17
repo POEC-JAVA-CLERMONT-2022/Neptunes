9d30913f27f4a3842ce008f53e7fa7e20ee37534
Lucas <lucas.crenais@gmail.com>
2022-03-17 12:05:11 +0100

    game class creation

---
+++ Projet/src/io/ipme/neptunes/Model/Game.java
@@ -0,0 +1,22 @@
+package io.ipme.neptunes.Model;
+
+public class Game {
+    private String gameUrl;
+    private boolean isPaused = false;
+
+    public String getGameUrl() {
+        return gameUrl;
+    }
+
+    public void setGameUrl(String gameUrl) {
+        this.gameUrl = gameUrl;
+    }
+
+    public boolean isPaused() {
+        return isPaused;
+    }
+
+    public void setPaused(boolean paused) {
+        isPaused = paused;
+    }
+}
