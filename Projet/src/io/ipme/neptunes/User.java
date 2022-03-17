e17a9f58d3ccb0347648bded09a874dfc0d7a592
Leo <leo.garand@outlook.fr>
2022-03-17 10:51:06 +0100

    User class creation

---
+++ Projet/src/io/ipme/neptunes/User.java
@@ -0,0 +1,49 @@
+package io.ipme.neptunes;
+
+public class User {
+	/*
+	 * 	Attributes
+	 */
+	private String userName;
+	private String email;
+	private String password;
+	private String avatar;
+	private Boolean isPremium;
+	
+	/*
+	 * 	Constructors
+	 */
+	public User(String userName, String email, String password, String avatar, Boolean isPremium) {
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+		this.avatar = avatar;
+		this.isPremium = isPremium;
+	}
+
+	/*
+	 * 	Getters
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public Boolean getIsPremium() {
+		return isPremium;
+	}
+	
+	
+	
+}
