da66f5dcac39ece0e28cebad07a3eda35b336cfc
Lucas <lucas.crenais@gmail.com>
2022-03-24 14:48:38 +0100

    add repository

---
+++ src/main/java/io/ipme/neptunes/Repository/GameRepository.java
@@ -0,0 +1,8 @@
+package io.ipme.neptunes.Repository;
+
+import io.ipme.neptunes.Model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Integer> {
+    Game findBy(Integer id);
+}
