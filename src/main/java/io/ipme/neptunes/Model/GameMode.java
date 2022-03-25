7f34387dfb19be2d899265c78a2bf03b3232a571
Imacookieftw <a.domingos44@gmail.com>
2022-03-25 09:58:57 +0100

    Game Mode Enum

--- src/main/java/io/ipme/neptunes/Model/GameMode.java
+++ src/main/java/io/ipme/neptunes/Model/GameMode.java
@@ -1,2 +1,16 @@
-package io.ipme.neptunes.Model;public enum GameMode {
+package io.ipme.neptunes.Model;
+
+public enum GameMode {
+
+    Normal ("Normal"), Random ("Random"), Faster ("Faster");
+
+    private String value;
+
+    GameMode(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
