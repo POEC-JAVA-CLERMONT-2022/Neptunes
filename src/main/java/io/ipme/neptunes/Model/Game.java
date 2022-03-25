f14b332d7ed3fcd06dbfe801e53ff40a51d7b046
Lucas <lucas.crenais@gmail.com>
2022-03-25 11:14:37 +0100

    handling game & playlist relationship

--- src/main/java/io/ipme/neptunes/Model/Game.java
+++ src/main/java/io/ipme/neptunes/Model/Game.java
@@ -1,10 +1,8 @@
 package io.ipme.neptunes.Model;
 
-import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.util.List;
-import java.util.UUID;
 
 @Entity
 public class Game {
@@ -42,6 +40,14 @@ public class Game {
         this.id = id;
     }
 
+    public String getGameUrl() {
+        return gameUrl;
+    }
+
+    public void setGameUrl(String gameUrl) {
+        this.gameUrl = gameUrl;
+    }
+
     public Boolean getPaused() {
         return isPaused;
     }
@@ -50,4 +56,22 @@ public class Game {
         isPaused = paused;
     }
 
+    public List<Score> getScore() {
+        return score;
+    }
+
+    public void setScore(List<Score> score) {
+        this.score = score;
+    }
+
+    public Playlist getPlaylist() {
+        return playlist;
+    }
+
+    public void setPlaylist(Playlist playlist) {
+        this.playlist = playlist;
+    }
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Playlist playlist;
 }
