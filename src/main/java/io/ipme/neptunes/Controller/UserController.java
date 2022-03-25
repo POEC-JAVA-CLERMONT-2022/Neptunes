7e1de264cf3381d525b87e91caf91c6e2a014ec7
Leo <leo.garand@outlook.fr>
2022-03-25 16:14:02 +0100

    UserCSR creation

--- src/main/java/io/ipme/neptunes/Controller/UserController.java
+++ src/main/java/io/ipme/neptunes/Controller/UserController.java
@@ -1,29 +1,23 @@
-/*package io.ipme.neptunes.Controller;
+package io.ipme.neptunes.Controller;
 
-import java.util.ArrayList;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.ipme.neptunes.Model.User;
 import io.ipme.neptunes.Service.UserService;
 
+import java.util.List;
+
 @RestController
 public class UserController {
 
-private UserService userService = new UserService();
+    @Autowired
+    private UserService userService;
 	
 	@GetMapping("/users")
-	public ArrayList<User> getUsers() {
-		return userService.getAll();
+	public List<User> findAll() {
+		return userService.findAll();
 	}
-	
-	
-	@GetMapping("/users/add")
-	public String addAnimal() {
-		for (int i = 0; i < 10; i++) {
-        	userService.Add(new User("pseudo "+i, "pseudo"+i+"@pseudo.com", "password"+i, "avatar"+i+".jpg", false));
-		}
-		return "OK"; 		
-	}
-}*/
+
+}
