5833db36a1ea4c2591d20c8e97985c35b0f06341
Leo <leo.garand@outlook.fr>
2022-03-29 14:09:54 +0200

    Theme CSR

--- src/main/java/io/ipme/neptunes/Service/ThemeService.java
+++ src/main/java/io/ipme/neptunes/Service/ThemeService.java
@@ -3,9 +3,7 @@ package io.ipme.neptunes.Service;
 import io.ipme.neptunes.Model.Theme;
 import io.ipme.neptunes.Repository.ThemeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,10 +18,7 @@ public class ThemeService {
 
     public Optional<Theme> findById(Integer id) { return themeRepository.findById(id); }
 
-    public ResponseEntity<Theme> createTheme(Theme theme) {
-        themeRepository.saveAndFlush(theme);
-        return ResponseEntity.ok().build();
-    }
+    public void createTheme(Theme theme) { themeRepository.saveAndFlush(theme); }
 
     public void deleteTheme(Integer id) { themeRepository.deleteById(id); }
 
