02232253bb74fe0322000cb511d1f52eafb85094
Imacookieftw <a.domingos44@gmail.com>
2022-03-29 11:47:00 +0200

    Game CSR Update

--- src/main/java/io/ipme/neptunes/Service/GameService.java
+++ src/main/java/io/ipme/neptunes/Service/GameService.java
@@ -6,12 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
-    @Service
+@Service
     public class GameService {
 
         @Autowired
         private GameRepository gameRepository;
 
-        public List<Game> findAll() { return gameRepository.findAll(); }
+        public List<Game> findAll() {
+            return gameRepository.findAll();
+        }
+
+        public Optional<Game> findOne(Integer id){
+            return gameRepository.findById(id);
+        }
+
+        public void createGame(Game game){
+            gameRepository.saveAndFlush(game);
+        }
+
+        public void deleteGame(Integer id){
+            gameRepository.deleteById(id);
+        }
     }
