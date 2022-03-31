88a505b9c3e6eb56a24458b95f6c99f1cd8ac3dc
Imacookieftw <a.domingos44@gmail.com>
2022-03-31 16:43:35 +0200

    App test

--- src/test/java/io/ipme/neptunes/NeptunesApplicationTests.java
+++ src/test/java/io/ipme/neptunes/NeptunesApplicationTests.java
@@ -1,14 +1,21 @@
 package io.ipme.neptunes;
 
 import io.ipme.neptunes.Model.Game;
+import io.ipme.neptunes.Model.GameMode;
+import io.ipme.neptunes.Model.UserGamePK;
 import io.ipme.neptunes.Repository.GameRepository;
+import io.ipme.neptunes.Repository.UserGameRepository;
 import io.ipme.neptunes.Service.GameService;
+import io.ipme.neptunes.Service.UserGameService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,7 +32,14 @@ class NeptunesApplicationTests {
     void gameTest(){
         //création d'une game test.
         Game game1 = new Game();
-        game1.setGameUrl("pouet");
+        game1.setGameUrl("neptunes/game1.com");
+        game1.setMode(GameMode.Normal);
+        game1.setPaused(false);
+        gameService.createGame(game1);
         gameRepository.saveAndFlush(game1);
+
+        Assertions.assertFalse(game1.getPaused(), "Game is paused.");
+        Assertions.assertNotNull(game1.getGameUrl(), "Game url is not set.");
+        Assertions.assertNotNull(game1.getMode(), "Game mode is not set.");
     }
 }
